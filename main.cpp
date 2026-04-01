58e5e55f99b0c3a85781b8df6297035e5ab3b04e
Sjoerd de Jonge <51705709+Sjoerd2497@users.noreply.github.com>
2026-04-01 23:11:59 +0200

    chore: git init

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/**
+ *  Music Library Indexer
+ *  @author Sjoerd de Jonge
+ *  2026-03-28
+ *
+ *  Reads .aiff music files in target folder
+ *  Extracts metadata
+ *  Structures metadata for the entire target folder
+ */
+
+
+// Audio IFF uses big-endian order, meaning the most significant byte is stored at the smallest memory address
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
