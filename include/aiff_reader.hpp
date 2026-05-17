dc7515f9ce5d3f3db8d74822f9715180ea7a7e81
Sjoerd de Jonge <51705709+sjoerddejonge@users.noreply.github.com>
2026-05-17 09:45:45 +0200

    style: Doxygen style comments for aiff_reader.hpp

--- include/aiff_reader.hpp
+++ include/aiff_reader.hpp
@@ -15,19 +15,28 @@
 
 
 namespace aiff {
-/// AIFF comment
+/// AIFF comment from the Comments Chunk. Contents of this struct should be included in the JSON.
 struct Comment {
+    // TODO: Include timestamp in Comment struct
+    /// ID of the marker that this comment is linked to, 0 if not linked to a marker.
     std::uint16_t marker_id;
+    /// Comment text.
     std::string text;
 };
 
-/// Metadata to be extracted from the AIFF file
+/// Metadata to be extracted from the AIFF file.
 struct Metadata {
+    /// The name chunk contains the name of the sampled sound.
     std::vector<uint8_t> name;                  // Name chunk
+    /// The author chunk contains one or more author names.
     std::vector<uint8_t> auth;                  // Author chunk
+    /// Contains a copyright notice for the sound.
     std::vector<uint8_t> copyright;             // Copyright chunk
+    /// Annotation chunk contains a comment.
     std::vector<uint8_t> anno;                  // Annotation chunk
+    /// Can contain multiple comments.
     std::vector<Comment> comments;              // Comment chunk(s)
+    /// The position of the ID3 tag on the ifstream.
     std::optional<std::streampos> id3_pos;      // Position of the ID3 chunk (start of data part)
 };
 
